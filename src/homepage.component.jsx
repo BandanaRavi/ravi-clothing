c29cb89623f8a88a5b309706a6885e05ab47406f
bandana <you@example.com>
2021-07-22 14:08:48 +0530

    create the first clothing uncomplete app

---
+++ src/homepage.component.jsx
@@ -0,0 +1,39 @@
+import './homepage.style.scss';
+const HomePage = () => (
+    <div className='homepage'>
+    <div className='main-menu'>
+    <div className='menu-item'>
+    <div className='content'>
+    <h1 className='title'>Hats</h1>
+    <span className='subtitle'>Shop Now</span>
+    </div>
+    </div>
+    <div className='menu-item'>
+    <div className='content'>
+    <h1 className='title'>Ja</h1>
+    <span className='subtitle'>Shop Now</span>
+    </div>
+    </div>
+    <div className='menu-item'>
+    <div className='content'>
+    <h1 className='title'>Hats2</h1>
+    <span className='subtitle'>Shop Now</span>
+    </div>
+    </div>
+    <div className='menu-item'>
+    <div className='content'>
+    <h1 className='title'>Hats3</h1>
+    <span className='subtitle'>Shop Now</span>
+    </div>
+    </div> 
+    <div className='menu-item'>
+    <div className='content'>
+    <h1 className='title'>Hats4</h1>
+    <span className='subtitle'>Shop Now</span>
+    </div>
+    </div>
+    </div>
+
+    </div>
+);
+export default HomePage;
